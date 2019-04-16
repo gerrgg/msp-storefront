2f98707f293b145e9dbd77fed5790c9a2c8b2afb
gregbast1994 <gregbast1994@gmail.com>
2019-04-16 08:46:05 -0400

    organize theme settings

--- functions.php
+++ functions.php
@@ -1,17 +1,38 @@
 <?php 
 
+//globals
+define('URI', get_stylesheet_directory_uri() );
+define('PATH', get_stylesheet_directory() );
+
+//require
+require_once( PATH . '/admin-functions.php' );
+
+/**
+ * Front-end Theme Settings
+ */
+
 class MSP{
     function __construct(){
         add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
     }
 
-
+    /**
+     * Enqueue scripts & css for child theme.
+     */
     public function enqueue_scripts(){
-        define('PATH', get_template_directory_uri() . '-child' );
+        // Custom javascript functions
+        wp_enqueue_script( 'msp', URI . '/inc/functions.js', array('jquery'), filemtime( __DIR__ . '\inc\functions.js' ), true );
 
-        wp_enqueue_style( 'bootstrap', PATH . '/vendor/bootstrap-4.3.1-dist/css/bootstrap.min.css' );
-        wp_enqueue_script( 'bootstrap', PATH . '/vendor/bootstrap-4.3.1-dist/js/bootstrap.bundle.min.js' );
+        //Twitter Bootstrap - https://getbootstrap.com/docs/4.3/getting-started/introduction/
+        wp_enqueue_style( 'bootstrap', URI . '/vendor/bootstrap-4.3.1-dist/css/bootstrap.min.css' );
+        wp_enqueue_script( 'bootstrap', URI . '/vendor/bootstrap-4.3.1-dist/js/bootstrap.bundle.min.js' );
+
+        //slideout.js - https://github.com/Mango/slideout
+        wp_enqueue_style( 'slideout', URI . '/vendor/slideout/dist/slideout.min.js' );
     }
+
 }
 
+
+//init
 new MSP();
---
+++ header.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package storefront
+ */
+?><!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php do_action( 'storefront_before_site' ); ?>
+
+<div id="page" class="hfeed site">
+	<?php do_action( 'storefront_before_header' ); ?>
+
+	<header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
+
+		<?php
+		/**
+		 * Functions hooked into msp_header action
+		 *
+
+		 */
+		do_action( 'msp_header' );
+		?>
+
+	</header><!-- #masthead -->
+
+	<?php
+	/**
+	 * Functions hooked in to storefront_before_content
+	 *
+	 * @hooked storefront_header_widget_region - 10
+	 * @hooked woocommerce_breadcrumb - 10
+	 */
+	do_action( 'storefront_before_content' );
+	?>
+
+	<div id="content" class="site-content" tabindex="-1">
+		<div class="col-full">
+
+		<?php
+		do_action( 'storefront_content_top' );
