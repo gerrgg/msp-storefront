1f871d2751f35b5131014b9114f005139d6d584e
gregbast1994 <gregbast1994@gmail.com>
2019-04-18 14:37:31 -0400

    init owl carousel

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,34 @@
 jQuery(document).ready(function( $ ){
+    var msp = {
+        init: function(){
+            msp.init_owl_carousel();
+        },
+
+        init_owl_carousel: function(){
+            console.log( 'init carousel' );
+            $('.owl-carousel').owlCarousel({
+                loop:true,
+                margin:10,
+                responsiveClass:true,
+                responsive:{
+                    0:{
+                        items:1,
+                        nav:true
+                    },
+                    600:{
+                        items:8,
+                        nav:false
+                    },
+                    1000:{
+                        items:14,
+                        nav:true,
+                        loop:false
+                    }
+                }
+            })
+        }
+    }
+
+    msp.init();
 
 });
