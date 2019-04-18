d24f73c5412a2f6461871559f2212169ce0f42b9
gregbast1994 <gregbast1994@gmail.com>
2019-04-18 11:38:46 -0400

    added owl carousel and bootstrap nav walker

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ define('URI', get_stylesheet_directory_uri() );
 define('PATH', get_stylesheet_directory() );
 
 //require
-require_once( PATH . '/vendor/class-wp-bootstrap-navwalker.php' );
+require_once( PATH . '/vendor/wp-bootstrap-navwalker-master/class-wp-bootstrap-navwalker.php' );
 require_once( PATH . '/admin-functions.php' );
 require_once( PATH . '/inc/msp-class-user-history.php' );
 require_once( PATH . '/inc/msp-template-hooks.php' );
@@ -55,6 +55,12 @@ class MSP{
         //Twitter Bootstrap - https://getbootstrap.com/docs/4.3/getting-started/introduction/
         wp_enqueue_style( 'bootstrap', URI . '/vendor/bootstrap-4.3.1-dist/css/bootstrap.min.css' );
         wp_enqueue_script( 'bootstrap', URI . '/vendor/bootstrap-4.3.1-dist/js/bootstrap.bundle.min.js' );
+
+        //Owl Carousel - https://owlcarousel2.github.io/OwlCarousel2/
+        wp_enqueue_style( 'owl-carousel', URI . '/vendor/OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css' );
+        wp_enqueue_style( 'owl-carousel-theme', URI . '/vendor/OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css' );
+
+        wp_enqueue_script( 'owl-carousel', URI . '/vendor/OwlCarousel2-2.3.4/dist/owl.carousel.min.js' );
 
         //slideout.js - https://github.com/Mango/slideout
         wp_enqueue_script( 'slideout', URI . '/vendor/slideout/dist/slideout.min.js', array(), 
