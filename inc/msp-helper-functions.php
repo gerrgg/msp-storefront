d57f712a2fa081c637e9a02f28742dd10b030b88
gregbast1994 <gregbast1994@gmail.com>
2019-04-26 12:46:01 -0400

    image helper

--- inc/msp-helper-functions.php
+++ inc/msp-helper-functions.php
@@ -7,6 +7,14 @@ function msp_get_product_image_src( $img_id ){
     return $src[0];
 }
 
+function msp_get_product_image_src_by_product_id( $product_id ){
+    $product = wc_get_product( $product_id );
+    $product_image_id = ( ! empty( $product ) ) ? $product->get_image_id() : 0;
+    
+    return ( ! empty( $product_image_id ) ) ? msp_get_product_image_src( $product_image_id ) : null;
+}
+
+
 function deslugify( $str ){
     return ucwords( str_replace( array('_', '-'), ' ', $str ) );
 }
