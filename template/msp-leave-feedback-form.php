1f306b7a0db67adf3bef8441c6897f720ff69db7
= <=>
2019-05-16 10:28:14 -0400

    allow users to edit store reviews

--- template/msp-leave-feedback-form.php
+++ template/msp-leave-feedback-form.php
@@ -3,10 +3,20 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
-
+$comment = msp_customer_feedback();
+$rating = get_comment_meta( $comment->comment_ID, 'rating', true );
+$_GET['star'] = $rating;
 ?>
 
 <form id="msp-feedback-form" class='d-flex flex-column justify-content-center align-items-center'>
+    <?php if( ! empty( $comment ) ) : ?>
+
+    <div class="form-group border-bottom">
+        <p class="lead">On <?php echo date( 'l, F jS', strtotime( $comment->comment_date ) ) ?> you gave the follow review:</p>
+    </div>
+
+    <?php endif; ?>
+
     <div class="form-group">
         <h3>How are we doing?</h3>
         <?php msp_get_review_more_star_buttons() ?>
@@ -14,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
     <div class="form-group w-75">
         <h4>What can we do to improve?</h4>
-        <textarea name="comments"></textarea>
+        <textarea name="comments"><?php echo ( isset( $comment->comment_content ) ) ? $comment->comment_content : ''; ?></textarea>
     </div>
     <input type="hidden" name="user_id" value="<?php echo get_current_user_id() ?>" />
     <input type="hidden" name="action" value="msp_process_feedback_form" />
