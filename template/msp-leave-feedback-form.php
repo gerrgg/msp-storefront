1b4873657371feabc0a0beacbff0c0e4610c1112
= <=>
2019-05-16 10:33:19 -0400

    decide what to do with this form

--- template/msp-leave-feedback-form.php
+++ template/msp-leave-feedback-form.php
@@ -3,6 +3,12 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
+/**
+ *  this form seems silly to attach to each order. 
+ *  We can either make this specific feedback on each order ( but will customers want to do that?)
+ *  Or we can detatch this form from the orders page and use for other applications. 
+ */ 
+
 $comment = msp_customer_feedback();
 $rating = get_comment_meta( $comment->comment_ID, 'rating', true );
 $_GET['star'] = $rating;
@@ -12,13 +18,13 @@ $_GET['star'] = $rating;
     <?php if( ! empty( $comment ) ) : ?>
 
     <div class="form-group border-bottom">
-        <p class="lead">On <?php echo date( 'l, F jS', strtotime( $comment->comment_date ) ) ?> you gave the follow review:</p>
+        <p class="lead">On <?php echo date( 'l, F jS Y', strtotime( $comment->comment_date ) ) ?> you gave the follow review:</p>
     </div>
 
     <?php endif; ?>
 
     <div class="form-group">
-        <h3>How are we doing?</h3>
+        <h3>How did we do on this order?</h3>
         <?php msp_get_review_more_star_buttons() ?>
     </div>
 
@@ -27,6 +33,7 @@ $_GET['star'] = $rating;
         <textarea name="comments"><?php echo ( isset( $comment->comment_content ) ) ? $comment->comment_content : ''; ?></textarea>
     </div>
     <input type="hidden" name="user_id" value="<?php echo get_current_user_id() ?>" />
+    <input type="hidden" name="order_id" value="<?php echo get_current_user_id() ?>" />
     <input type="hidden" name="action" value="msp_process_feedback_form" />
     <div class="alert alert-warning feedback" role="alert"></div>
     <button type="submit" class="btn btn-success btn-block">Submit Feedback</button>
