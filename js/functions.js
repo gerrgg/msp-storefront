6fd00a5aaddb333620df0d4d333063e0035f7b99
= <=>
2019-05-23 08:41:27 -0400

    include functionality to convert url to json
    also, getting prepared for ajax ship filters ( and more ajax if pjax works well )

--- js/functions.js
+++ js/functions.js
@@ -5,6 +5,19 @@ jQuery(document).ready(function( $ ){
             msp.bind_karma_buttons();
             $(document.body).on( 'click', 'i.msp-star-rating', msp.bind_create_review_star_buttons )
             $('#msp_review').on( 'click', '.remove-product-image-from-review', msp.delete_user_product_image )
+            $('#shop-filters').on( 'click', '.woocommerce-widget-layered-nav-list__item > a', msp.get_product_query )
+        },
+
+        get_product_query: function( e ){
+            e.preventDefault();
+            console.log( e );
+            let search = e.target.search;
+            var obj = msp.get_json_from_url(search);
+            console.log( obj );
+
+            $.post( e.target.href, obj, function( data ){
+                console.log( data );
+            } );
         },
 
         delete_user_product_image: function( e ){
@@ -84,6 +97,37 @@ jQuery(document).ready(function( $ ){
 
             });
         },
+
+        get_json_from_url: function(url) {
+            // https://stackoverflow.com/questions/8486099/how-do-i-parse-a-url-query-parameters-in-javascript
+
+            if( ! url ) url = location.href;
+            var question = url.indexOf("?");
+            var hash = url.indexOf("#");
+            if(hash==-1 && question==-1) return {};
+            if(hash==-1) hash = url.length;
+            var query = question==-1 || hash==question+1 ? url.substring(hash) : 
+            url.substring(question+1,hash);
+            var result = {};
+            query.split("&").forEach(function(part) {
+              if(!part) return;
+              part = part.split("+").join(" "); // replace every + with space, regexp-free version
+              var eq = part.indexOf("=");
+              var key = eq>-1 ? part.substr(0,eq) : part;
+              var val = eq>-1 ? decodeURIComponent(part.substr(eq+1)) : "";
+              var from = key.indexOf("[");
+              if(from==-1) result[decodeURIComponent(key)] = val;
+              else {
+                var to = key.indexOf("]",from);
+                var index = decodeURIComponent(key.substring(from+1,to));
+                key = decodeURIComponent(key.substring(0,from));
+                if(!result[key]) result[key] = [];
+                if(!index) result[key].push(val);
+                else result[key][index] = val;
+              }
+            });
+            return result;
+          }
     }
 
     msp.init();
