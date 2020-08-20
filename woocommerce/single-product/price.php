00fee2b803b55aa17d462b8af7b9c954b6b8a1f7
Gregory Bastianelli <greg@gerrg.com>
2020-08-20 15:33:54 -0400

    add free-shipping message

---
+++ woocommerce/single-product/price.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Single Product Price
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/price.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+global $product;
+
+?>
+<p class="<?php echo esc_attr( apply_filters( 'woocommerce_product_price_class', 'price' ) ); ?>">
+
+<?php echo $product->get_price_html(); ?>
+
+<?php if( $product->get_price() > 100 && $product->get_shipping_class_id() != get_option( 'woo_ltl_shipping_class_id' ) && $product->get_shipping_class_id() != get_option( 'woo_ups_only_shipping_class_id' ) ) : ?>
+<span style="color: #333; font-size: .75rem">+ Free Shipping</span>
+<?php endif; ?>
+
+</p>
